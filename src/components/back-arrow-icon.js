21b4e511b12c1d0dc24b41b6ad4aa8e6052c5806
Matt Hagner <web@matthagner.com>
2018-12-31 15:49:40 -0600

    Svg components

---
+++ src/components/back-arrow-icon.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function BackArrowIcon({ size = 50 } = {}) {
+  return (
+    <svg
+      version="1.1"
+      id="emoji"
+      xmlns="http://www.w3.org/2000/svg"
+      height={size}
+      width={size}
+      x="0px"
+      y="0px"
+      viewBox="0 0 72 72"
+      enableBackground="new 0 0 72 72"
+    >
+      <polygon
+        id="_x2B05__xFE0F_"
+        fill="#3F3F3F"
+        stroke="#1D1D1B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        points="
+        22.8,51.5 5,35 22.8,18.5 26.6,22.6 16.2,32.3 67,32.3 67,37.8 16.2,37.8 26.6,47.5 "
+      />
+    </svg>
+  );
+}
