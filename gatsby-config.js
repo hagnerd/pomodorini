2a28ed4973ea0f2047b1eb1c8390cafced489258
Matt Hagner <web@matthagner.com>
2018-12-31 15:46:55 -0600

    Conif baby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
+  ],
+};
