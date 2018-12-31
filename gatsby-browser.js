4e6ea24d3e5ec859a356f46c19ae49e0b66d9366
Matt Hagner <web@matthagner.com>
2018-12-31 15:48:06 -0600

    Document

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-/* Setup react hot loader for react hooks */
-import { setConfig } from 'react-hot-loader'
+import { setConfig } from 'react-hot-loader';
 
+/* 1. Setup react hot loader for react hooks */
 setConfig({ pureSFC: true });
